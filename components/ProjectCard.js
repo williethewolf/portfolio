a39a6403233fa19d016ce49b68076532e3580a21
willieochoa <willieochoa+ga@gmail.com>
2023-01-17 15:20:32 -0800

    WIP: Added language percentages to project cards from github's API

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,9 +1,65 @@
-export default function ProjectCard({name,thumbnail,description,url, index, languageAPI}) {
-    //console.log(languageAPI)
+import {useState, useEffect} from 'react';
+import ProgressBar from 'react-progressbar';
+
+export default function ProjectCard({name,thumbnail,description,url, index}) {
+    const [languages, setLanguages] = useState({});
+
+    const languageColors = {
+        JavaScript: '#f1e05a',
+        TypeScript: '#007acc',
+        CSS: '#563d7c',
+        HTML: '#e34c26',
+        Python: '#3572A5',
+        Ruby: '#701516',
+        Java: '#b07219',
+        C: '#555555'
+        // add more language-color mappings as needed
+      };
+  
+    useEffect(() => {
+            async function fetchLanguages() {
+            const response = await fetch(`https://api.github.com/repos${url.slice(18)}languages`);
+            const data = await response.json();
+            setLanguages(data);
+            }
+            fetchLanguages();
+        }, [url]);
+
+  let totalLines = 0;
+  for (const [language, lines] of Object.entries(languages)) {
+    totalLines += lines;
+  }
+
+  const progressBars = (
+    <div className="relative w-full flex">
+      {Object.entries(languages).map(([language, lines]) => {
+        const percentage = (lines / totalLines) * 100;
+        if (percentage < 5 && percentage > 1) {
+            percentage = 5;
+          }
+        else if (percentage < 1) return null
+        return (
+          <div key={language} className="relative flex-row" style={{width:`${percentage}%`,}}>
+            <ProgressBar
+            //   className="absolute top-0 left-0"
+              completed={100}
+              color={languageColors[language] || '#333'}
+            />
+            <p className="absolute top-0 left-0.5 center text-xs font-medium text-white" style={{}}>{language}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
   return (
-    <div key={index} className="max-w-m bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 place-content-center">
+    <div key={index} className="max-w-m bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 place-content-center text-left">
         {/* Working on adding language usage from the Git hub API */}
-        {/* <div>{String(languageAPI)} here</div> */}
+        
+        
+        <div className=''>
+            <p className='text-xs place-content-left'>Languages used:</p>
+          {progressBars}
+        </div>
         <a href={url}>
             <img className="shadow-xl" src={thumbnail} alt={name} />
         </a>
--- components/projects_data.js
+++ components/projects_data.js
@@ -1,31 +1,47 @@
+export function getLanguages(url) {
+  return fetch(`https://api.github.com/repos${url.slice(18)}languages`)
+      .then(response => response.json())
+      .catch(error => console.log(error))
+}
+
 export default [
   {
     name:
       "Steam off",
     thumbnail: "https://github.com/williethewolf/SteamOff/raw/master/ReadMe_Assets/Screenshot%202022-09-06%20170516.png",
     description:
-    <p>How many times have you opened Steam, just to endlessly and aimlessly browse your library just to end up not playing anything at all because you don't have enough time left?
-
-      Take the indecisiveness out of the equation. Shake up you library to come across titles you've forgotten or didn't even know you had. Or just let STEAM Off take that weight off your shoulders and pick a game for you!
-      
-      Kick back, input your Steam Id64, pick random games until you find one you like or straight up open up a surprise.
-      
-      It's time to let off some STEAM©!</p>,
+    <div>
+      <p>
+        How many times have you opened Steam, just to endlessly and aimlessly browse your library just to end up not playing anything at all because you don't have enough time left?
+      </p>
+      <br />
+      <p>
+        Take the indecisiveness out of the equation. Shake up you library to come across titles you've forgotten or didn't even know you had. Or just let STEAM Off take that weight off your shoulders and pick a game for you!
+      </p> 
+      <br />
+      <p>
+        Kick back, input your Steam Id64, pick random games until you find one you like or straight up open up a surprise.
+      </p> 
+      <br />
+      <p>
+        It's time to let off some STEAM©!
+      </p>
+    </div>,
     url: "https://github.com/williethewolf/SteamOff/",
-    get languageAPI(){this.url.slice(18)}
+    languages: getLanguages('https://github.com/williethewolf/SteamOff/')
   },
   {
     name:
       "Plant Parenthood",
     thumbnail: "https://github.com/williethewolf/plant_parenthood/raw/master/main_app/static/imgs/screenshots/signin.png",
     description:
+    <div>
     <p>Plant Parenthood is an app that allows Plant Parent to organize, tend, learn, and share their favorite indoor plants.
-    learn more in our <a href="https://docs.google.com/presentation/d/1w9EXJ40Zl0EQq0UZT1XZ10TUgkHsbPInjGRPclx0slQ">slide show.</a></p>,
+    learn more in our <a href="https://docs.google.com/presentation/d/e/2PACX-1vSAaqhuh-KtbeN8il7ZPaVLY3Innj-9jPo4eSaOcjkO4ku9dFri1Atp74qm2s4i2xUgy_z2i-uA6nXU/pub?start=false&loop=false&delayms=5000" target="_blank" className="font-bold">slide show.</a>
+    </p>
+    </div>,
     url: "https://github.com/williethewolf/plant_parenthood/",
-    get languageAPI(){fetch(`https://api.github.com/repos${this.url.slice(18)}languages`)
-                      .then(response => response.json())
-                      
-                      }
+    languages: getLanguages("https://github.com/williethewolf/plant_parenthood/")
   },
   {
     name:
@@ -34,21 +50,23 @@ export default [
     description:
     <p>A minimalistic Kanban based project manager full stack app.</p>,
     url: "https://github.com/williethewolf/kanbas/",
-    get languageAPI(){fetch(`https://api.github.com/repos${this.url.slice(18)}languages`)
-                      .then(response => response.json())
-                      }
+    languages: getLanguages("https://github.com/williethewolf/kanbas/")
   },
   {
     name:
       "Pirates of The Silicon Sea",
     thumbnail: "https://github.com/williethewolf/Pirates-of-the-Silicon-Sea/raw/main/assets/Screenshots/ipad_Screenshot.png",
     description:
-    <p>A multiplayer naval combat game for 2 players, playing locally, sharing the device. 
-      Taking turns, the players attack each other until one of the ship's hull health is 0. Sails work as a buffer against damage with 30% chance of receiving damage. If sail's health reaches 0, all the damage will be absorbed by the hull.</p>,
+    <div>
+    <p>A multiplayer naval combat game for 2 players, playing locally, sharing the device.
+    </p>
+    <br />
+    <p> 
+      Taking turns, the players attack each other until one of the ship's hull health is 0. Sails work as a buffer against damage with 30% chance of receiving damage. If sail's health reaches 0, all the damage will be absorbed by the hull.
+    </p>
+    </div>,
     url: "https://github.com/williethewolf/Pirates-of-the-Silicon-Sea/",
-    get languageAPI(){fetch(`https://api.github.com/repos${this.url.slice(18)}languages`)
-                      .then(response => response.json())
-                      }
+    languages: getLanguages("https://github.com/williethewolf/Pirates-of-the-Silicon-Sea/")
   },
   {
     name:
@@ -57,9 +75,7 @@ export default [
     description:
     <p>A tycoon type clicker browser mini game.</p>,
     url: "https://github.com/williethewolf/Landscaper-clicker-minigame/",
-    get languageAPI(){fetch(`https://api.github.com/repos${this.url.slice(18)}languages`)
-                      .then(response => response.json())
-                      }
+    languages: getLanguages("https://github.com/williethewolf/Landscaper-clicker-minigame/")
   },
   
 ];
