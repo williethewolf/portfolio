39b335c118dd76af67b0665d9e8383ff89db4b4f
willieochoa <willieochoa+ga@gmail.com>
2023-01-23 19:25:10 -0800

    WIP: moved 'View in GIThub button to the right

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -75,11 +75,13 @@ export default function ProjectCard({name,thumbnail,description,url, index}) {
                 {progressBars}
             </div>
             <div className="relative py-1">
-        </div>
-            <a href={url} target="_blank" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-slate-500 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 dark:bg-blue-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800">
-                View in Gitub
-                <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-            </a>
+            </div>
+            <div className=' text-right'>
+                <a href={url} target="_blank" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-slate-500 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 dark:bg-blue-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800">
+                    View in Gitub
+                    <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                </a>
+            </div>
         </div>
     </div>
     
