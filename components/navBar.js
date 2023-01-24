ac2487e35f40548e3aa17afe65b5e9c59ddbd4a3
willieochoa <willieochoa+ga@gmail.com>
2023-01-23 19:23:42 -0800

    WIP: removed Magic Word modal to its own component

--- components/navBar.js
+++ components/navBar.js
@@ -1,17 +1,16 @@
-import React from "react"
+import {useEffect, useState} from "react"
 import Image from "next/image"
 import IDElogo from "../public/images/Visual_Studio_Code_1.35_icon.svg"
 
 //Modal imports
-import Draggable from 'react-draggable';
-import restrictedGif from "../public/images/restricted.gif"
+import MagicWordModal from "./MagicWordModal";
 import restrictedaudio from '../public/audio/magicword.mp3'
 import restrictedaudioLoop from '../public/audio/magicword_loop.mp3'
 
 export default function Navbar() {
 
 //Sticky navbar
-    const [sticky, setSticky] = React.useState(false)
+    const [sticky, setSticky] = useState(false)
 
     function setStickied(){
         const navbar = document.getElementById('Navigation')
@@ -29,43 +28,46 @@ export default function Navbar() {
     }
 
 //Modal Display
-    const [showModal, setShowModal] = React.useState(false);
+    const [showModal, setShowModal] = useState(false);
+
+ //Audio playback
+
+ const [audio, setAudio] = useState(null)
 
     
-//Audio playback
+ useEffect(() => {
+ setAudio(new Audio(restrictedaudio))
+ // only run once on the first render on the client
+ }, [])
 
-    const [audio, setAudio] = React.useState(null)
-    React.useEffect(() => {
-    setAudio(new Audio(restrictedaudio))
-    // only run once on the first render on the client
-    }, [])
+ const [audioPlaying, setAudioPlaying] = useState(false)
 
-    const [audioPlaying, setAudioPlaying] = React.useState(false)
+ useEffect(() => {
+     if (audio)
+     audioPlaying ? audio.play() : audio.pause(), audio.currentTime = 0;
+   },
+   [audioPlaying]
+ );
 
-    React.useEffect(() => {
-        if (audio)
-        audioPlaying ? audio.play() : audio.pause(), audio.currentTime = 0;
-      },
-      [audioPlaying]
-    );
+ const audioToggle = () => setAudioPlaying(!audioPlaying)
 
-    const audioToggle = () => setAudioPlaying(!audioPlaying)
+ useEffect(() => {
+    if (audio) {
+      audio.addEventListener("ended", () => {
+        const newAudio = new Audio(restrictedaudioLoop);
+        setAudio(newAudio);
+        newAudio.currentTime = 0;
+        newAudio.play();
+      });
+    }
+  }, [audio]);
 
-    React.useEffect(() => {
-        if (audio){
-        audio.addEventListener('ended', () => {setAudio(new Audio(restrictedaudioLoop), this.currentTime=0, this.play() )});
-        return () => {
-          audio.removeEventListener('ended', () => setAudioPlaying(false));
-        }
-    };
-      }, []);
-    
 
 //Fullscreen gimmick
-const [isFullscreen, setIsFullscreen] = React.useState(false);
+const [isFullscreen, setIsFullscreen] = useState(false);
 
 // Watch for fullscreenchange
-React.useEffect(() => {
+useEffect(() => {
   function onFullscreenChange() {
     setIsFullscreen(Boolean(document.fullscreenElement));
   }
@@ -79,45 +81,7 @@ React.useEffect(() => {
         <>
             {showModal ? (
                
-            <>
-            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                <Draggable
-                >
-                    <div  className="border-0 shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                        <div className="bg-stone-300  py-1 flex justify-between">
-                            <span className="pl-3">Magic_word.exe</span>
-                            <ul className="flex justify-end">
-                                <li className="">
-                                            <button className='bg-stone-300  px-2 hover:bg-stone-600 px-2'>-</button>
-                                            <button className="bg-stone-300  px-2 hover:bg-stone-600 px-2">□</button>
-                                            <button className="bg-stone-300  px-2 hover:bg-stone-600 px-2" onClick={() => { audioToggle(); setShowModal(false) }}>x</button>
-                                </li>
-                            </ul>
-                        </div>
-                        <div>
-                        <Image
-                                priority
-                                src= {restrictedGif}
-                            
-                                alt="Restricted"
-                            />  
-                        </div>
-                        {/* Uncomment if considered annoying to give an easier close button */}
-                            {/* <button
-                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                type="button"
-                                onClick={() => {setShowModal(false); audioToggle() }}
-                            >
-                                Close
-                            </button>               */}
-                        </div>
-                        </Draggable> 
-                </div>
-                
-            </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-            </>
+               <MagicWordModal audioToggle={audioToggle} setShowModal={setShowModal}/>
             
         ) : null}
 
